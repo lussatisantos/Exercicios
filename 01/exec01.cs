8a61b7d7408643474495579c77ef00bbee7bab45
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-16 17:28:19 +0000

    multiplicando numeros

---
+++ 01/exec01.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Principal{
+    static void Main(){
+        int num1, num2;
+        num1 = 19;
+        num2 = 42;
+
+        Console.WriteLine("A multiplicacao de " + num1 + " e " + num2 + " sera igual a " + num1*num2);
+    }
+}
