07ed952781e02a6faedba6622930cc02aeb3dd0c
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-20 15:28:16 +0000

    formatacao do console

---
+++ 02/exec02.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Exercicio02 {
+
+    static void Main(){
+        int num1, num2, num3;
+        num1 = 10;
+        num2 = 20;
+        num3 = 30;
+
+        Console.WriteLine(" O primeiro numero é {0},\n O segundo numero é {1}, \n O terceiro numero é {2}", num1, num2, num3);
+    }
+}
