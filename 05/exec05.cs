edf15fd8053206b23695acabc816301d10c29447
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-24 11:41:50 +0000

    tabuada Completa

--- 05/exec05.cs
+++ 05/exec05.cs
@@ -3,7 +3,7 @@ using System;
 class Exercicio05 {
 
     static void Main() {
-        int num, opc;
+        int num, opc, nada;
 
         Console.WriteLine("................................");
         Console.WriteLine("... TABUADA DE MULTIPLICACAO ...");
@@ -14,12 +14,37 @@ class Exercicio05 {
 
         Console.WriteLine("Pretendes saber a tabuada de: {0}", num);
 
-        
+        Console.WriteLine("1 - Multiplicao");
+        Console.WriteLine("2 - Divisao");
+        Console.WriteLine("3 - Adicao");
+        Console.WriteLine("4 - Subtracao");
 
-        for(int i = 1; i < 13; i++){
-            Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+        Console.Write("Digite uma opcao: ");
+        opc = Convert.ToInt32(Console.ReadLine());
+
+        if (opc == 1) {
+            for(int i = 1; i < 13; i++){
+                Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+            }
+        } else if (opc == 2) {
+            for(int i = 1; i < 13; i++){
+                Console.WriteLine("{0} x {1} = {2}", num, i, num / i);
+            }
+        } else if (opc == 3) {
+            for(int i = 1; i < 13; i++){
+                Console.WriteLine("{0} x {1} = {2}", num, i, num + i);
+            }
+        } else if (opc == 4) {
+            for(int i = 1; i < 13; i++){
+                Console.WriteLine("{0} x {1} = {2}", num, i, num - i);
+            }
+        } else {
+            Console.WriteLine("Opcao invalida, por favor digite uma opcao valida!!!");
         }
 
         Console.WriteLine("................................");
-    }   
+    
+        nada = Convert.ToInt32(Console.ReadLine());
+    }
+    
 }
