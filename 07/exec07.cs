960c0d6b06dad9dbceb75c7c7285853b973860dc
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-25 21:02:44 +0000

    extracao base dos dados

---
+++ 07/exec07.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Exercicio07 {
+    static void Main() {
+        int a, b, c;
+
+        Console.WriteLine("............................");
+        Console.WriteLine("...... EQUACAO 2 GRAU ......");
+        Console.WriteLine("............................");
+
+        Console.Write("Calcule o valor de a [x2]: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Calcule o valor de b [x]: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Calcule o valor de c [c]: ");
+        c = Convert.ToInt32(Console.ReadLine());
+
+        
+    }
+}
