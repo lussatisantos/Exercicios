b91780d2f95d086f510247b8e02799e74a3624ff
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-30 22:03:50 +0000

    Create exec08.cs

---
+++ 08/exec08.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Exercicio08 {
+    static void Main() {
+
+        int [,] matriz = new int[4, 3];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+            
+            }
+
+        }
+    }
+}
