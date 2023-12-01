f4b3b0288d07b60fea102ebc1a279f36c61bb757
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-12-01 06:46:17 +0000

    matriz

--- 08/exec08.cs
+++ 08/exec08.cs
@@ -3,13 +3,33 @@ using System;
 class Exercicio08 {
     static void Main() {
 
-        int [,] matriz = new int[4, 3];
+        int [,] matriz = new int[4, 4];
+
+        matriz[0, 0] = 10;
+        matriz[0, 1] = 20;
+        matriz[0, 2] = 30;
+        matriz[0, 3] = 40;
+
+        matriz[1, 0] = 11;
+        matriz[1, 1] = 12;
+        matriz[1, 2] = 13;
+        matriz[1, 3] = 14;
+
+        matriz[2, 0] = 21;
+        matriz[2, 1] = 22;
+        matriz[2, 2] = 23;
+        matriz[2, 3] = 24;
+
+        matriz[3, 0] = 31;
+        matriz[3, 1] = 32;
+        matriz[3, 2] = 33;
+        matriz[3, 3] = 34;
 
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 3; j++) {
-            
+            for (int j = 0; j < 4; j++) {
+                Console.Write("{0} ", matriz[i, j]);
             }
-
+            Console.Write("\n");
         }
     }
 }
