28414cc7e4a2743427240c34c945468fb1bfea9d
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-12-03 16:18:08 +0000

    capital

--- 10/exec10.cs
+++ 10/exec10.cs
@@ -7,6 +7,17 @@ class Exercicio10 {
         Console.Write("Digite a capital de Angola: ");
         resp = Console.ReadLine();
 
-        Console.WriteLine(resp);
+        if(resp == "luanda" || resp == "Luanda" || resp == "luandA" || resp == "lUanda"){
+            Console.WriteLine("Parabens voce acertou!!!");
+        }else if(resp == "luamda" || resp == "Luamda" || resp == "luamdA" || resp == "lUamda") {
+            Console.WriteLine("Estas de parabens, mas tenha atencao na grafia LUANDA e nao LUAMDA");
+        } else {
+            Console.WriteLine("Desculpe, mas voce ERROU");
+        }
+
+        /*
+            Exercicio proposto por Anita Lopes e Guto Garcia no livro
+            500 Algoritmos Resolvidos pag77/78;
+        */
     }
 }
