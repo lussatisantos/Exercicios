5c5da0880c9d623e0b758438efa4a3238bc83ee4
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-12-03 16:07:41 +0000

    .

---
+++ 10/exec10.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Exercicio10 {
+    static void Main() {
+        string resp = "";
+
+        Console.Write("Digite a capital de Angola: ");
+        resp = Console.ReadLine();
+
+        Console.WriteLine(resp);
+    }
+}
