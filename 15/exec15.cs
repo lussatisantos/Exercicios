0b600ef41ab89dafff3ce8ae04e02cf32484433e
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-12-09 14:08:58 +0000

    desenvolvendo o desafio OOP

--- 15/exec15.cs
+++ 15/exec15.cs
@@ -31,11 +31,28 @@ class Exercicio15 {
         equipa1.jogadores = 23;
         equipa1.nome = "Barcelona";
 
-        if(equipa1.treinador == true) {
+        if(equipa1.treinador != false) {
             Console.Write("Como se chama o treinador da equipa: ");
             string nomeTreinador = Console.ReadLine();
+
+        } else if (equipa1.treinador == false) {
+            Console.Write("A equipa possui um treinador interino? [S/N]: ");
+            string opc = Console.ReadLine();
+
+            if(opc == "S"){
+                equipa1.treinador = true;
+            } else if(opc == "N"){
+                Console.WriteLine("A equipa precisa de treinador ou um adjunto do clube devera ser o treinador interino");
+            } else {
+                Console.WriteLine("Opcao invalida, por favor digite uma opcao valida");
+            }
         }
 
+        if (equipa1.adjunto != true) {
+            Console.WriteLine("Um treinador precisa de Adjuntos para ajuda-lo a tomar as melhores decisoes para a equipa");
+        } else {
+            Console.WriteLine("A equipa possui adjuntos");
+        }
         Console.WriteLine(equipa1.nome);
     }
 }
