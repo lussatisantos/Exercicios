7bd9eadc4808450b63bfbd8765b426123be013b7
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-12-10 12:41:08 +0000

    OOP

--- 15/exec15.cs
+++ 15/exec15.cs
@@ -23,6 +23,7 @@ class Exercicio15 {
         //Console.WriteLine(eq1.treinador);
 
         Equipa1();
+        Equipa2();
     }
 
     public static void Equipa1() {
@@ -54,5 +55,36 @@ class Exercicio15 {
             Console.WriteLine("A equipa possui adjuntos");
         }
         Console.WriteLine(equipa1.nome);
+    }
+
+    public static void Equipa2() {
+        Equipa equipa2 = new Equipa();
+
+        equipa2.jogadores = 18;
+        equipa2.nome = "Sporting CP";
+
+        if(equipa2.treinador != false) {
+            Console.Write("Como se chama o treinador da equipa: ");
+            string nomeTreinador = Console.ReadLine();
+
+        } else if (equipa2.treinador == false) {
+            Console.Write("A equipa possui um treinador interino? [S/N]: ");
+            string opc = Console.ReadLine();
+
+            if(opc == "S"){
+                equipa2.treinador = true;
+            } else if(opc == "N"){
+                Console.WriteLine("A equipa precisa de treinador ou um adjunto do clube devera ser o treinador interino");
+            } else {
+                Console.WriteLine("Opcao invalida, por favor digite uma opcao valida");
+            }
+        }
+
+        if (equipa2.adjunto != true) {
+            Console.WriteLine("Um treinador precisa de Adjuntos para ajuda-lo a tomar as melhores decisoes para a equipa");
+        } else {
+            Console.WriteLine("A equipa possui adjuntos");
+        }
+        Console.WriteLine(equipa2.nome);
     }
 }
