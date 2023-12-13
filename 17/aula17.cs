458150b983fe45e017cdc5aabbd66f1f638e65a4
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-12-13 07:54:25 +0000

    sobrecarga do construtor do Objecto

--- 17/aula17.cs
+++ 17/aula17.cs
@@ -8,7 +8,7 @@ class Carro {
     public void info() {
         Console.WriteLine("A marca do carro.....: {0}", marca);
         Console.WriteLine("A rodas do carro.....: {0}", rodas);
-        Console.WriteLine("A velocidade do carro: {0}", velocidade);
+        Console.WriteLine("A velocidade do carro: {0}\n", velocidade);
     }
 
     public Carro() {
@@ -16,12 +16,28 @@ class Carro {
         rodas = 4;
         velocidade = 320;
     }
+
+    public Carro(int v) {
+        marca = "Mercedes";
+        rodas = 4;
+        velocidade = v;
+    }
+
+    public Carro(string nome, int v) {
+        marca = nome;
+        rodas = 4;
+        velocidade = v;
+    }
 }
 
 class Aula17 {
     static void Main() {
-        Carro carro = new Carro();
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro(200);
+        Carro carro3 = new Carro("Lamborghini", 400);
 
-        carro.info();
+        carro1.info();
+        carro2.info();
+        carro3.info();
     }
 }
