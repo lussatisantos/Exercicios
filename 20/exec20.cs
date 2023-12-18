5f1c4714c91498b193f04b3736cac25dfc5286d3
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-12-18 09:59:57 +0000

    .

---
+++ 20/exec20.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Home {
+    public int bedroom;
+    public int sitting;
+    public int kitchen;
+    public int bath;
+
+    public Home() {
+        bedroom = 1;
+        sitting = 1;
+        kitchen = 1;
+        bath = 1;
+    }
+
+    public void info() {
+        Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        Console.WriteLine("-=-=-=-=-=-=-=-  WELCOME AT HOME  -=-=-=-=-=-=-=-");
+        Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    }
+}
+
+class Exercise20 {
+    static void Main() {
+
+        Home home1 = new Home();
+
+        home1.info();
+    }
+}
