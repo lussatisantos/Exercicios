a95bc8e3aa3541bb219dba3ed7c9abaa940a2f6b
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2024-01-04 16:37:27 +0000

    classe sealed

---
+++ 29/exec29.cs
@@ -0,0 +1,19 @@
+using System;
+
+sealed class Veiculo {
+    public int teste;
+    protected int teste1;
+    private int teste2;
+}
+
+/*
+    class Carro:Veiculo {
+
+    }
+*/
+
+class Exercicio29 {
+    static void Main() {
+
+    }
+}
