17b486bf5828be0283367d5c3434f021cb316cb8
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2024-01-23 08:32:35 +0000

    metodo recursivo

--- 38/exec38.cs
+++ 38/exec38.cs
@@ -1,7 +1,22 @@
 using System;
 
+class Mat {
+    public int fatorial(int num){
+        int resultado = num;
+        if(resultado < 1){
+            resultado = 1;
+        } else {
+            resultado *= fatorial(resultado - 1);
+        }
+
+        return resultado;
+    }
+}
+
 class Exercicio38 {
     static void Main(){
-           
+        Mat mat = new Mat();
+
+        Console.WriteLine(mat.fatorial(5)); 
     }
 }
